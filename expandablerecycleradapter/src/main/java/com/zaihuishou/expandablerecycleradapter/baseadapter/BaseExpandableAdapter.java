b54b633377f0fe0acf8915e0b6fb4d8c4ab7159d
tantan <tanzhiqiang@creivc.com>
2016-07-14 09:52:10 +0800

    modify package name

--- expandablerecycleradapter/src/main/java/com/zaihuishou/expandablerecycleradapter/baseadapter/BaseExpandableAdapter.java
+++ expandablerecycleradapter/src/main/java/com/zaihuishou/expandablerecycleradapter/baseadapter/BaseExpandableAdapter.java
@@ -1,4 +1,4 @@
-package com.zaihuishou.expandablerecycleradapter.adapter;
+package com.zaihuishou.expandablerecycleradapter.baseadapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
